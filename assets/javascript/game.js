1eed5e2b22d7dc0f348b8af69804e8a3ec57e3e1
isaac korff <yikorff@gmail.com>
2019-01-23 17:04:27 -0700

    adding unit-4-game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,67 @@
+$(document).ready(function () {
 
+    var userTotal = 0;
+    var wins = 0;
+    var losses = 0;
+    var youwon = "";
+    var youlost = "";
+
+    $(".pokeballs").each(function () {
+        var random = Math.floor(Math.random() * 11 + 1);
+        $(this).attr("data-value", random);
+    });
+
+
+    var max = 120;
+    var min = 19;
+    var compTotal = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    $("#computer").append(compTotal);
+
+
+    $(".pokeballs").click(function () {
+
+        var value = $(this).attr("data-value");
+        userTotal += parseInt(value);
+
+        $("#user").text(userTotal)
+
+        if (userTotal === compTotal) {
+            $("#youwon").text("You Won");
+            wins++;
+            $("#wins").text("wins" + " " + wins);
+            reset()
+
+
+
+        }
+
+        if (userTotal > compTotal) {
+            $("#lost").text("You Lost!");
+            losses++;
+            $("#losses").text("losses" + " " + losses);
+            reset()
+        }
+
+    });
+
+    function reset() {
+        userTotal = 0;
+        $("#user").text(userTotal);
+        compTotal = Math.floor(Math.random() * (max - min + 1)) + min;
+        $("#computer").text(compTotal);
+
+        $(".pokeballs").each(function () {
+            var random = Math.floor(Math.random() * 11 + 1);
+            $(this).attr("data-value", random);
+        });
+
+        setTimeout(function () {
+            $("#youwon").text("")
+            $("#lost").text("")
+        }, 1000);
+
+    };
+
+
+});
